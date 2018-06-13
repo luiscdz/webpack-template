1cb656387f10ef439f6778db63b61a6114e8b91a
luiscdz <luisedu.cdz@gmail.com>
2018-06-13 11:53:28 -0500

    Uso de loader, Ej archivos css

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');// modulo que permite el manejo de rutas relativas
+
+
+module.exports = {
+  entry: path.resolve(__dirname,'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      }
+    ]
+  }
+}
