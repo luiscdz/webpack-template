6cbf84a26dfaa3abbaf99cf704fbc3ed188a4e51
luiscdz <luisedu.cdz@gmail.com>
2018-06-13 11:30:17 -0500

    configuraciones de webpack externas

---
+++ external/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');// modulo que permite el manejo de rutas relativas
+
+
+module.exports = {
+  entry: path.resolve(__dirname,'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
