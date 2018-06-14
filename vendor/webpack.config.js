186a9de677d0c8acf54d8410e29b662a51bc37e5
luiscdz <luisedu.cdz@gmail.com>
2018-06-14 18:05:26 -0500

    vendor pack

---
+++ vendor/webpack.config.js
@@ -0,0 +1,135 @@
+const path = require('path');// modulo que permite el manejo de rutas relativas
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    vendor:[
+      'react',
+      'react-dom'
+    ],
+    home: path.resolve(__dirname,'src/js/index.js'),
+    contacts: path.resolve(__dirname,'src/js/contacts.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: './dist/'
+  },
+  optimization: {
+    splitChunks: { 
+      cacheGroups: {
+        default: false,
+        commons: {
+          name: 'vendor',
+          chunks: 'all',
+          minChunks: Infinity
+        }
+      }
+    },
+  },
+  module: {
+    rules: [
+      /* {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          //'style-loader',
+          'css-loader'
+        ],
+      },  */
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          //'style-loader',
+          'css-loader',
+          'sass-loader'
+          
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              noIeCompat: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          //'style-loader',
+          'css-loader',
+          {
+            loader: 'stylus-loader',
+            options:{
+              user: [ 
+                require('nib'),
+                require('rupture')
+              ],
+              import: [
+                '~nib/lib/nib/index.styl',
+                '~rupture/rupture/index.styl'
+              ]
+            }
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png|gif|woff|ttf|svg|eot)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000000
+          }
+        }
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000000,
+            name: 'video/[name].[hash].[ext]',
+
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].css"
+    })
+  ]
+ // watch: true
+}
